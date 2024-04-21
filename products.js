6c160488b9ff6a311aab91a78d9dc85b216b418d
shivam <shivamsingh.bimt@gmail.com>
2024-04-21 21:43:17 +0530

    cart created

---
+++ products.js
@@ -0,0 +1,8 @@
+const productList = [
+    {id: 1, name: "Product-1", price:100},
+    {id: 2, name: "Product-2", price:200},
+    {id: 3, name: "Product-3", price:300},
+    {id: 4, name: "Product-4", price:400},
+    {id: 5, name: "Product-5", price:500},
+    {id: 6, name: "Product-6", price:600}
+];
